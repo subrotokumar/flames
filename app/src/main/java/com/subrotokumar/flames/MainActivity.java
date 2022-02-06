0d9095b894a11f98641f8e6c62175fd22e3cb319
Subroto Kumar <subrotokumar@outlook.in>
2022-02-06 20:14:03 +0530

    replace findbyviewid with view binding

--- app/src/main/java/com/subrotokumar/flames/MainActivity.java
+++ app/src/main/java/com/subrotokumar/flames/MainActivity.java
@@ -9,42 +9,49 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.subrotokumar.flames.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private ArrayList<String> a=new ArrayList<>();
+    private String flames[]={"SIBLING","FRIEND","LOVER","AFFECTIONATE","MARRIAGE","ENEMY"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void showresult(View view)
     {
-        String flames[]={"SIBLING","FRIEND","LOVER","AFFECTIONATE","MARRIAGE","ENEMY"};
-        ArrayList<String> a=new ArrayList<>();
+        a=new ArrayList<>();
         for(String st:flames)
             a.add(st);
-        String s1=((EditText)findViewById(R.id.nameinput1)).getText().toString().toUpperCase();
-        String s2=((EditText)findViewById(R.id.nameinput2)).getText().toString().toUpperCase();
+        String s1=binding.nameinput1.getText().toString().toUpperCase();
+        String s2=binding.nameinput2.getText().toString().toUpperCase();
         if(s1.length()>0&&s2.length()>0) {
             int ch1[] = new int[24];
             int ch2[] = new int[24];
             for (int i = 0; i < s1.length(); i++) {
                 char ch=s1.charAt(i);
-                if (ch>=65&&ch<=90)
-                    ch1[(int) (ch - 'A')]++;
+                if (Character.isAlphabetic(ch))
+                    ch1[(int)ch-65]++;
             }
             for (int i = 0; i < s2.length(); i++) {
                 char ch=s2.charAt(i);
-                if (ch>=65&&ch<=90)
-                    ch2[(int)(ch - 'A')]++;
+                if (Character.isAlphabetic(ch))
+                    ch2[(int)ch-65]++;
             }
             int sum = 0;
             for (int i = 0; i < 24; i++)
                 sum += (Math.abs(ch1[i] - ch2[i]));
             if(sum==0)
-                ((TextView) findViewById(R.id.result)).setText("COMPLICATED");
+                binding.result.setText("COMPLICATED");
             else {
                 int pos = 0;
                 while (a.size() > 1) {
@@ -52,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
                     a.remove(pos);
                     pos--;
                 }
-                ((TextView) findViewById(R.id.result)).setText(a.get(0));
+                binding.result.setText(a.get(0));
+                a.clear();
             }
         }
         else {
