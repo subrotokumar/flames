d82da6ab4b7d64440435814859b4628225e46681
Subroto Kumar <subrotokumar@outlook.in>
2022-02-13 00:51:41 +0530

    Solved App crash issue

--- app/src/main/java/com/subrotokumar/flames/MainActivity.java
+++ app/src/main/java/com/subrotokumar/flames/MainActivity.java
@@ -3,6 +3,7 @@ package com.subrotokumar.flames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +17,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private ArrayList<String> a=new ArrayList<>();
     private String flames[]={"SIBLING","FRIEND","LOVER","AFFECTIONATE","MARRIAGE","ENEMY"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
@@ -29,42 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void showresult(View view)
     {
-        a=new ArrayList<>();
-        for(String st:flames)
-            a.add(st);
-        String s1=binding.nameinput1.getText().toString().toUpperCase();
-        String s2=binding.nameinput2.getText().toString().toUpperCase();
-        if(s1.length()>0&&s2.length()>0) {
-            int ch1[] = new int[24];
-            int ch2[] = new int[24];
-            for (int i = 0; i < s1.length(); i++) {
-                char ch=s1.charAt(i);
-                if (Character.isAlphabetic(ch))
-                    ch1[(int)ch-65]++;
-            }
-            for (int i = 0; i < s2.length(); i++) {
-                char ch=s2.charAt(i);
-                if (Character.isAlphabetic(ch))
-                    ch2[(int)ch-65]++;
-            }
-            int sum = 0;
-            for (int i = 0; i < 24; i++)
-                sum += (Math.abs(ch1[i] - ch2[i]));
-            if(sum==0)
-                binding.result.setText("COMPLICATED");
-            else {
-                int pos = 0;
-                while (a.size() > 1) {
-                    pos = (pos + sum) % a.size();
-                    a.remove(pos);
-                    pos--;
+        try {
+            ArrayList<String> a=new ArrayList<>();
+            for (String st : flames)
+                a.add(st);
+            String s1 = binding.nameinput1.getText().toString().toUpperCase();
+            String s2 = binding.nameinput2.getText().toString().toUpperCase();
+            //Log.d("Name Display","\n"+s1+"\n"+s2);
+            if (s1.length() > 0 && s2.length() > 0) {
+                int ch1[] = new int[24];
+                int ch2[] = new int[24];
+                for (int i = 0; i < s1.length(); i++) {
+                    char ch = s1.charAt(i);
+                    if (Character.isAlphabetic(ch))
+                        ch1[(int) (ch - 'A')]++;
                 }
-                binding.result.setText(a.get(0));
-                a.clear();
+                for (int i = 0; i < s2.length(); i++) {
+                    char ch = s2.charAt(i);
+                    if (Character.isAlphabetic(ch))
+                        ch2[(int) (ch - 'A')]++;
+                }
+                int sum = 0;
+                for (int i = 0; i < 24; i++)
+                    sum += (Math.abs(ch1[i] - ch2[i]));
+                if (sum == 0)
+                    binding.result.setText("COMPLICATED");
+                else {
+                    int pos = 0;
+                    while (a.size() > 1) {
+                        pos = (pos + sum) % a.size();
+                        a.remove(pos);
+                        pos--;
+                    }
+                    binding.result.setText(a.get(0));
+                    a.clear();
+                }
+            } else {
+                Toast.makeText(this, "Please enter the name!!", Toast.LENGTH_SHORT).show();
             }
         }
-        else {
-            Toast.makeText(this, "Please enter the name!!", Toast.LENGTH_SHORT).show();
+        catch (Exception e){
+            //Log.e("App Crash", String.valueOf(e));
+            e.printStackTrace();
         }
     }
 
