f83d69e80b9951a0d07dad52619d0cd34af2a01c
Subroto Kumar <subrotokumar@outlook.in>
2022-02-14 00:17:41 +0530

    Minor bugs and Issue fixed

--- app/src/main/java/com/subrotokumar/flames/MainActivity.java
+++ app/src/main/java/com/subrotokumar/flames/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private String flames[]={"SIBLING","FRIEND","LOVER","AFFECTIONATE","MARRIAGE","ENEMY"};
+    private String flames[]={"SIBBLING","FRIEND","LOVER","AFFECTIONATE","MARRIAGE","ENEMY"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,53 +26,54 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-    }
 
-    public void showresult(View view)
-    {
-        try {
-            ArrayList<String> a=new ArrayList<>();
-            for (String st : flames)
-                a.add(st);
-            String s1 = binding.nameinput1.getText().toString().toUpperCase();
-            String s2 = binding.nameinput2.getText().toString().toUpperCase();
-            //Log.d("Name Display","\n"+s1+"\n"+s2);
-            if (s1.length() > 0 && s2.length() > 0) {
-                int ch1[] = new int[24];
-                int ch2[] = new int[24];
-                for (int i = 0; i < s1.length(); i++) {
-                    char ch = s1.charAt(i);
-                    if (Character.isAlphabetic(ch))
-                        ch1[(int) (ch - 'A')]++;
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    ArrayList<String> a = new ArrayList<>();
+                    for (String st : flames)
+                        a.add(st);
+                    String s1 = binding.nameinput1.getText().toString().toUpperCase();
+                    String s2 = binding.nameinput2.getText().toString().toUpperCase();
+
+                    if (s1.length() > 0 && s2.length() > 0) {
+                        int ch1[] = new int[26];
+                        int ch2[] = new int[26];
+                        for (int i = 0; i < s1.length(); i++) {
+                            char ch = s1.charAt(i);
+                            if (ch >= 'A' && ch <= 'Z')
+                                ch1[(int)ch - 65]++;
+                        }
+                        for (int i = 0; i < s2.length(); i++) {
+                            char ch = s2.charAt(i);
+                            if (ch >= 'A' && ch <= 'Z')
+                                ch2[(int)ch - 65]++;
+                        }
+                        int sum = 0;
+                        for (int i = 0; i < 26; i++)
+                            sum += Math.abs(ch1[i]-ch2[i]);
+                        if (sum == 0)
+                            binding.result.setText("COMPLICATED");
+                        else {
+                            int pos = 0;
+                            while (a.size() > 1) {
+                                pos = (pos + sum) % a.size();
+                                a.remove(pos);
+                                pos--;
+                            }
+                            binding.result.setText(a.get(0));
+                            Log.d("Name Display", "\n" + s1 + "\n" + s2 + " " + a.get(0));
+                            a.clear();
+                        }
+                    } else
+                        Toast.makeText(MainActivity.this, "Please enter the name!!", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Log.e("App Crash", String.valueOf(e));
+                    e.printStackTrace();
                 }
-                for (int i = 0; i < s2.length(); i++) {
-                    char ch = s2.charAt(i);
-                    if (Character.isAlphabetic(ch))
-                        ch2[(int) (ch - 'A')]++;
-                }
-                int sum = 0;
-                for (int i = 0; i < 24; i++)
-                    sum += (Math.abs(ch1[i] - ch2[i]));
-                if (sum == 0)
-                    binding.result.setText("COMPLICATED");
-                else {
-                    int pos = 0;
-                    while (a.size() > 1) {
-                        pos = (pos + sum) % a.size();
-                        a.remove(pos);
-                        pos--;
-                    }
-                    binding.result.setText(a.get(0));
-                    a.clear();
-                }
-            } else {
-                Toast.makeText(this, "Please enter the name!!", Toast.LENGTH_SHORT).show();
             }
-        }
-        catch (Exception e){
-            //Log.e("App Crash", String.valueOf(e));
-            e.printStackTrace();
-        }
+        });
     }
 
     public void clear(View view) {
